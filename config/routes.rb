5f862472a889c55651812c8c6964a8f55d8c8236
ryarata <arata.ryo19921112@gmail.com>
2015-03-25 11:38:38 +0900

    なんかいろいろ追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
+  root 'home#index'
+
   resources :admins
 
   # The priority is based upon order of creation: first created -> highest priority.
